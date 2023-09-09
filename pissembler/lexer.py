82dcdf0d3e809d22b516e3454ec315bdbc05045e
rik61072@gmail.com <rik61072@gmail.com>
2023-09-09 15:06:45 +0530

    feat: hex and decimal literals work

--- pissembler/lexer.py
+++ pissembler/lexer.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 from typing import List, Optional
 from pydantic import BaseModel
+import re
 
 class token_type(Enum):
     PUSH1 = auto()
@@ -12,7 +13,7 @@ class token_type(Enum):
 
 class Token(BaseModel):
     t_type: token_type
-    value: str
+    value: Optional[str] = None
 
 class LexerEdge(BaseModel):
     value: Optional[str]
@@ -32,7 +33,6 @@ class Lexer:
         self.state = LexerEdge(value=None, is_start=True)
         for instruction, instruction_enum in instructions:
             curr = self.state
-            prev = None
             length = len(instruction)
             for index, character in enumerate(instruction):
                 skip = False
@@ -54,24 +54,57 @@ class Lexer:
         curr = self.state
         result = []
         length = len(text)
-        for index, character in enumerate(text):
+        operand = {"value": None, "type": None}
+        literal_rules = {
+            token_type.DECIMAL_LITERAL: re.compile(r"[0-9]"),
+            token_type.HEX_LITERAL: re.compile(r"([0-9])|([A-Z])|([a-z])")
+        }
+        index = 0
+        # for index, character in enumerate(text):
+        while index < length:
+            character = text[index]
             change = False
             is_end = (index == (length - 1))
             if character.isspace():
                 if not curr.is_start:
                     if curr.t_type is None:
                         raise Exception(f"Incomplete symbol at position {index+1}")
-                    result.append(curr.t_type)
+                    result.append(Token(t_type=curr.t_type))
                     curr = self.state
+                elif operand["value"] is not None:
+                    result.append(Token(t_type=operand["type"], value=operand["value"]))
+                    operand = {"value": None, "type": None}
+                index += 1
                 continue
+            if operand["value"] is not None:
+                if bool(literal_rules[operand["type"]].match(character)):
+                    operand["value"] += character
+                    change = True
+                else:
+                    raise Exception(f"Unidentified literal rule at position {index+1}")
             for edge in curr.next_edges:
                 if edge.value == character:
                     curr = edge
                     change = True
                     break
             if is_end and (not character.isspace()):
-                    result.append(curr.t_type)
-                    curr = self.state
+                if operand["value"] is not None:
+                    result.append(Token(t_type=operand["type"], value=operand["value"]))
+                    break
+                result.append(Token(t_type=curr.t_type))
+                curr = self.state
+            if not change and operand["value"] is None:
+                if character == "0" and text[index+1]:
+                    operand["value"] = "0x"
+                    operand["type"] = token_type.HEX_LITERAL
+                    index += 1
+                    change = True
+                elif character.isdigit():
+                    operand["value"] = character
+                    operand["type"] = token_type.DECIMAL_LITERAL
+                    change = True
+
             if not change:
                 raise Exception(f"Unsupported symbol probably at position {index+1}")
+            index += 1
         return result
